2494e4a128a901a897df5537930b2d6ce13fae46
famnas321 <famnaspk380@gmail.com>
2025-02-18 18:28:28 +0530

    first commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      <h1 className="text-3xl font-bold underline bg-gray-500">
+      Hello world!
+    </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
