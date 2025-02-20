4be27a731e04e56edcd983dff94d264944e1481d
famnas321 <famnaspk380@gmail.com>
2025-02-20 12:03:33 +0530

    committed on 20.2.2025

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 // import './App.css'
 import SignIn from './accounts/SignIn'
 import SignUp from './accounts/SignUp'
+import Pending from './components/Pending'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<SignIn/>}/>
         <Route path='/register' element={<SignUp/>}/>
+        <Route path='/pending' element={<Pending/>}/>
       </Routes>
    
   )
