16aa874609c24683046995f4f99dd5c14cb5b773
famnas321 <famnaspk380@gmail.com>
2025-03-09 23:23:19 +0530

    committed on 9.3.2025

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -4,7 +4,7 @@ import {heroui} from "@heroui/theme"
 module.exports = {
   content: [
     "./index.html",
-    "./src//*.{ts,tsx,js,jsx}",
+    "./src/**/*.{ts,tsx,js,jsx}",
     './src/layouts//*.{js,ts,jsx,tsx,mdx}',
     './src/pages//*.{js,ts,jsx,tsx,mdx}',
     './src/components//*.{js,ts,jsx,tsx,mdx}',
