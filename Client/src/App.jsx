090339ccb554b6a209e05ea453d043c01b708574
famnas321 <famnaspk380@gmail.com>
2025-03-11 07:41:17 +0530

    committed on 11.2.2025

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -12,7 +12,7 @@ import Messages from './components/messages/messages'
 import Contact from './components/contacts/contact'
 import Addvertisment from './components/addvertisment/addvertisment'
 import Profiles from './components/profile/profiles'
-
+import Addpackage from './components/posts/Addpackage'
 
 
 
@@ -35,6 +35,8 @@ function App() {
           <Route path='contact-us' element={<Contact/>}/>
           <Route path='advertisments' element={<Addvertisment/>}/>
           <Route path='profile' element={<Profiles/>}/>
+          <Route path='/addPackage' element={<Addpackage/>}/>
+
 
         </Route>
         
