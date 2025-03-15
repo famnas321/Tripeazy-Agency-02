6c7f43094a1f94d2130539ddc7e51c12440135c7
famnas321 <famnaspk380@gmail.com>
2025-03-15 15:41:16 +0530

    committed for

--- Client/src/components/posts/Addpackage.jsx
+++ Client/src/components/posts/Addpackage.jsx
@@ -264,7 +264,7 @@ function Addpackage() {
     
   </form>
   <label htmlFor="" className= " mt-2 text-gray-700 font-medium">
-        Package Over view 
+       Package Over View
       </label>
 
   <textarea
@@ -275,6 +275,8 @@ function Addpackage() {
     className=" w-full h-32  py-2.5 sm:py-3 px-4 block  border border-gray-200 rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" 
     onChange={handleChange}
   />
+  
+ 
 
 <button 
   type="submit"
