e26c955b981328e34dce725b1cf2748c7fe351d8
famnas321 <famnaspk380@gmail.com>
2025-04-15 18:24:39 +0530

    committed on 15.4.2025

--- Client/src/components/RestrictRoute.jsx
+++ Client/src/components/RestrictRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate } from 'react-router-dom';
 
 function RestrictRoute({ children }) {
     const authData = useSelector((state) => state.auth.userInfo);
-
+   console.log(authData,"this is from restrict route")
     if (!authData) {
         return <Navigate to="/login" replace />;
     }
