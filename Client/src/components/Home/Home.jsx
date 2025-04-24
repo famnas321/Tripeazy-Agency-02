2b2f111ceb6af4193937dbef6c19123c3d0beffd
famnas321 <famnaspk380@gmail.com>
2025-04-25 01:10:01 +0530

    committed on 25.4.2025

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -4,27 +4,27 @@ import { Component } from "./DashboardCards";
 import AccessPopup from "../AccessPopup";
 
 function Home() {
-  const [showPopup, setShowPopup] = useState(false); // Initially hidden
+  // const [showPopup, setShowPopup] = useState(false); 
 
-  const handlePopup = () => {
-    if (!showPopup) {
-      setShowPopup(true);
-    }
-  };
+  // const handlePopup = () => {
+  //   if (!showPopup) {
+  //     setShowPopup(true);
+  //   }
+  // };
 
-  const handleClosePopup = () => {
-    setShowPopup(false);
-  };
+  // const handleClosePopup = () => {
+  //   setShowPopup(false);
+  // };
 
   return (
-    <div className="flex flex-col min-h-screen" onClick={handlePopup}>
+    <div className="flex flex-col min-h-screen" >
       <NavHome />
       <div className="flex-grow p-6 mt-20">
         <Component />
         <h1 className="mt-6 text-xl font-bold">It's Home Page</h1>
       </div>
 
-      {showPopup && <AccessPopup onClose={handleClosePopup} />}
+      {/* {showPopup && <AccessPopup onClose={handleClosePopup} />} */}
     </div>
   );
 }
